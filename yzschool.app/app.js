bfbdf2247707c83de69bb3764ab77447550cb01a
xiongdaxionger <xiongerxiongda@gmail.com>
2017-07-19 10:16:19 +0800

    add

---
+++ yzschool.app/app.js
@@ -0,0 +1,38 @@
+//app.js
+App({
+  onLaunch: function() {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+
+  getUserInfo: function(cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.getUserInfo({
+        withCredentials: false,
+        success: function(res) {
+          that.globalData.userInfo = res.userInfo
+          typeof cb == "function" && cb(that.globalData.userInfo)
+        }
+      })
+    }
+  },
+
+  onShareAppMessage: function () {
+    return {
+      title: '微信小程序联盟',
+      desc: '最具人气的小程序开发联盟!',
+      path: '/page/user?id=123'
+    }
+  },
+
+
+  globalData: {
+    userInfo: null,
+      }
+})
